cd9fcf75d49c340450ecc977d8bd58074b1b5c57
Alan Rutter <alanrutter@Alans-MacBook-Air.local>
2014-08-14 17:37:13 +0100

    Changed routes to move session and add houses

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,12 @@ Rails.application.routes.draw do
 
   get 'sessions/new'
 
-  get 'session/create'
-
-  get 'session/new'
-
-  get 'session/destroy'
-
 	root "houses#index"
 
 	resources :houses
 
 	resources :users do 
-		resource :session
+		resource :houses
 	end
 
 	resources :session
