7c39d8a0179e117493d18764683d5749167e66c5
Alan Rutter <alanrutter@Alans-MacBook-Air.local>
2014-08-14 15:06:12 +0100

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+House.create([{
+	title: "Alan's house", 
+	address: "31 Effingham Road, London N8 0AA",
+	latitude: 51.584539,
+	longitude: -0.104937,
+	price_in_pence: 6000,
+	number_of_rooms: 1,
+	max_guests: 2,
+	is_featured: true,
+	user_id: 1	
+}])
